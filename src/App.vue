116e8f1ebef60f25e376d2b16c64af66732a7ced
lxw <lxw@git.com>
2019-01-02 00:01:30 +0800

    first commit
    async test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  let test = async A => {
+    console.log("start ")
+    A().then(n => {
+      console.log(n)
+    })
+
+   await createPromise();
+
+
+    console.log("end ")
   }
-}
+
+  let funcA = async () => {
+    console.log("a1")
+
+    new Promise(resolve=> {
+      console.log("a4")
+      resolve()
+    }).then(()=>{
+      console.log("a5")
+    })
+
+
+    return 8;
+  }
+
+  let createPromise = ()=>{
+    return new Promise(resolve => {
+        console.log("a3")
+        resolve(9)
+    })
+  }
+
+  export default {
+    name: 'app',
+    created(){
+      this.init()
+    },
+    methods:{
+      init(){
+        test(funcA)
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
